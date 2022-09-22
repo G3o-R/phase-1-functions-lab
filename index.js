470c606bd66a7aabe5c35e41df720cd8361c53f4
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2022-09-21 20:06:56 -0700

    commit tree 6ea9e0d58d9d63483a758ad8f2496f32f519a183

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if (someValue===43) {
+        return 1
+     }
+     else if (someValue===50,34) {
+        return 8
+     }
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue)*264
+}
+
+function distanceTravelledInFeet(start,destination){
+    return (Math.abs(start-destination))*264
+}
+
+function calculatesFarePrice(start,destination){
+    const distance = Math.abs(start-destination)
+    if(((distance)*264)<400){
+        return 0
+    }
+    else if (((distance)*264)>400 &&((distance)*264)<2000 ){
+        return (((distance)*264)-400)*.02
+    }
+    else if (((distance)*264)>2000 && ((distance)*264)<2500){
+        return 25
+    }
+    else if((distance*264)>2500){
+        return 'cannot travel that far'
+    }
+}
